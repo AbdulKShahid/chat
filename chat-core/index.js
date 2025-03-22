23eb298af2ba927432b57607f429769bca8daac5
abdulkshahid <abdulkshahid@gmail.com>
2025-03-22 23:47:06 +0100

    added postgres config

--- chat-core/index.js
+++ chat-core/index.js
@@ -1,11 +1,25 @@
 
 const express = require('express'); //Import the express dependency
 const app = express();              //Instantiate an express app, the main work horse of this server
-const port = 5000;                  //Save the port number where your server will be listening
-
+const port = 3000;                  //Save the port number where your server will be listening
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+const {pool} = require('./db.js');
 //Idiomatic expression in express to route and respond to a client request
-app.get('/', (req, res) => {        //get requests to the root ("/") will route here
-    res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
+app.get('/', (req, res) => {     
+    
+    pool.query('SELECT * FROM users', (error, results) => {
+        if (error) {
+          throw error
+        }
+        res.json(results.rows);
+    })
+      //get requests to the root ("/") will route here
                                                         //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
 });
 
