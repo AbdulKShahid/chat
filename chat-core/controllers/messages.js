cce1dac4acefc38879992a6bf6144548bbc9a24e
abdulkshahid <abdulkshahid@gmail.com>
2025-03-23 23:10:18 +0100

    added users and messages routes, controllers, endpoints

---
+++ chat-core/controllers/messages.js
@@ -0,0 +1,21 @@
+const {pool} = require('../db.js');
+exports.list = (req,res) => {
+
+    pool.query('SELECT * FROM messages', (error, results) => {
+        if (error) {
+          throw error
+        }
+        res.json(results.rows);
+    })
+}
+
+
+exports.create = (req,res) => {
+
+    pool.query('INSERT INTO messages ()', (error, results) => {
+        if (error) {
+          throw error
+        }
+        res.json(results.rows);
+    })
+}
--- chat-core/index.js
+++ chat-core/index.js
@@ -3,25 +3,17 @@ const express = require('express'); //Import the express dependency
 const app = express();              //Instantiate an express app, the main work horse of this server
 const port = 3000;                  //Save the port number where your server will be listening
 const bodyParser = require('body-parser')
+const messagesRouter = require('./routes/messages.js');
+const usersRouter = require('./routes/users.js');
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 )
-const {pool} = require('./db.js');
-//Idiomatic expression in express to route and respond to a client request
-app.get('/', (req, res) => {     
-    
-    pool.query('SELECT * FROM users', (error, results) => {
-        if (error) {
-          throw error
-        }
-        res.json(results.rows);
-    })
-      //get requests to the root ("/") will route here
-                                                        //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
-});
+
+app.use('/messages', messagesRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
     console.log(`Now listening on port ${port}`); 
